876ddf6155b3475bfb190b2c8684836a7a7735c6
Yaremith  Damian <laudamian27@gmail.com>
2020-04-04 13:11:43 -0600

    Modificaciones en el ContentView

--- NetworkingDemoApp/ContentView.swift
+++ NetworkingDemoApp/ContentView.swift
@@ -8,9 +8,58 @@
 
 import SwiftUI
 
+struct Response: Codable{
+    var results: [Result]
+    
+}
+struct Result: Codable{
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
 struct ContentView: View {
+    
+    @State private var results=[Result]()
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List{
+                ForEach(results, id: \.trackId){item in
+                    VStack(alignment: .leading){
+                        Text(item.trackName)
+                            .font(.headline)
+                        Text(item.collectionName)
+                    }
+                }
+                
+                
+            }.onAppear(perform: loadData)
+            .navigationBarTitle("Song List")
+            
+        }
+    }
+    
+    
+    func loadData(){
+        guard let url = URL (string:"https://itunes.apple.com/search?term=taylor+swift&entity=song") else{
+          print("Invalid URL")
+          return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request){data, response,  error in
+            
+            if let data = data{
+                if let decodeResponse = try? JSONDecoder().decode(Response.self, from: data){
+                    DispatchQueue.main.async {
+                       self.results=decodeResponse.results
+                    }
+                    return
+                    
+                }
+            }
+            print("Fetch Failed: \(error?.localizedDescription ?? "Unknow Error")")
+            
+        }.resume()
     }
 }
 
